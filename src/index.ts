5d0ba9c71fedd23ab9fdb52f3d246f080ca8ded3
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-03-28 15:42:50 +1100

    start

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-function areAnagrams(s1: string, s2: string) {
-  const charCount = new Map<string, number>();
-  for (const char of s1.split('')) {
-    charCount.set(char, (charCount.get(char) || 0) + 1);
-  }
-  for (const char of s2.split('')) {
-    if (!charCount.has(char)) return false;
-    charCount.set(char, charCount.get(char) - 1);
-  }
-  return Array.from(charCount.values())
-    .every(val => val === 0);
-}
+const foo = [
+    "Alpha",
+    "beta",
+    "Gama"
+]
+foo.sort();
+console.log(foo);
